b8b562fecfe124de8f51d9e3f4a4106ef1d4ae2a
voteit.top <76503907+voteit-top@users.noreply.github.com>
2021-01-21 10:12:39 +0800

    Update voteitdetails.js

--- voteitdetails.js
+++ voteitdetails.js
@@ -1,1 +1,7 @@
 
+var rankItemDetails = {
+  0: {desc:"this is a short description",imgs:"image1.jpg|image2.jpg"},
+  //key is itemId
+  1: {desc:"this is a short description of USA",imgs:"usa1.jpg|usa2.jpg"},
+  
+};
