ac29bc88d2ba0751a51c12ae749eef6e58f2ac4e
voteit.top <76503907+voteit-top@users.noreply.github.com>
2021-01-22 10:27:32 +0800

    Update voteitdetails.js

--- voteitdetails.js
+++ voteitdetails.js
@@ -2,6 +2,6 @@
 var rankItemDetails = {
   0: {desc:"this is a short description",imgs:"image1.jpg|image2.jpg"},
   //key is itemId
-  29: {desc:"Shanghai is finance center of China",imgs:"shanghai1.jpg|shanghai2.jpg|shanghai2.jpg"},
+  29: {desc:"Shanghai is finance center of China",imgs:"shanghai1.jpg|shanghai2.jpg|shanghai3.jpg"},
   
 };
