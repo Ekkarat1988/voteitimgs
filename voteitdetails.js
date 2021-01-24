e01351d52e9ab060098dbad5911b93f52a9c9bb4
voteit.top <76503907+voteit-top@users.noreply.github.com>
2021-01-24 09:46:11 +0800

    Update voteitdetails.js

--- voteitdetails.js
+++ voteitdetails.js
@@ -2,6 +2,6 @@
 var rankItemDetails = {
   0: {desc:"this is a short description",imgs:"image1.jpg|image2.jpg"},
   //key is itemId
+  6: {desc:"Shanghai is finance center of China",imgs:"china-denys-nevozhai.jpg|china-ling-tang.jpg|china-ren-ran.jpg|china-wu-yi.jpg|china-sam-beasley.jpg"},
   29: {desc:"Shanghai is finance center of China",imgs:"shanghai1.jpg|shanghai2.jpg|shanghai3.jpg"},
-  
 };
