8895c47af63d3e17565512ed3d12b80e23dc948c
voteit.top <76503907+voteit-top@users.noreply.github.com>
2021-02-01 11:58:24 +0800

    update einstain

--- voteitdetails.js
+++ voteitdetails.js
@@ -2,7 +2,8 @@
 var rankItemDetails = {
   0: {desc:"this is a short description",imgs:"image1.jpg|image2.jpg"},
   //key is itemId
-  1:{desc:"USA is a country in north america.",imgs:"usa-andrew-hubbert.jpg|usa-josh-miller.jpg"},
+  5:{desc:"Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics).His work is also known for its influence on the philosophy of science. He is best known to the general public for his mass–energy equivalence formula E = mc2",
+     imgs:"einstein1.jpg|einstein2.jpg"},
   6: {desc:"China is a country in east asia..",imgs:"china-denys-nevozhai.jpg|china-ling-tang.jpg|china-ren-ran.jpg|china-wu-yi.jpg|china-sam-beasley.jpg"},
   29: {desc:"Shanghai is finance center of China",imgs:"shanghai1.jpg|shanghai2.jpg|shanghai3.jpg"},
 };
