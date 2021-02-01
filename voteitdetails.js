19f63dad5bfd03268a4a4f48d9fbae1962ba35a9
voteit.top <76503907+voteit-top@users.noreply.github.com>
2021-02-01 13:34:23 +0800

    update newton

--- voteitdetails.js
+++ voteitdetails.js
@@ -7,4 +7,6 @@ var rankItemDetails = {
   48: {desc:"Mao Zedong (1893-1976), also known as Chairman Mao, was a Chinese communist revolutionary who was the founder of the People's Republic of China (PRC), which he ruled as the chairman of the Chinese Communist Party from its establishment in 1949 until his death in 1976. Ideologically a Marxist–Leninist, his theories, military strategies, and political policies are collectively known as Maoism",
        imgs:"Mao-Zedong.jpg|Mao_Zedong_in_1959.jpg|Mao-Zedong1.jpg"},
   13: {desc:"Founder of TRX and TRON Network",imgs:"justin-sun.jpg"},
+  18:{desc:"Sir Isaac Newton PRS (1642-1726) was an English mathematician, physicist, astronomer, theologian, and author who is widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution.",
+     imgs:"Isaac_Newton.jpg|Isaac_Newton2.jpg|isaac-newton-granger.jpg"}
 };
